6cb5a9fb5cc1d79ed865a5f5a6b1864ccf81bc39
Jeffrey Hu <jeffrey@moveparallel.com>
2022-10-25 15:30:20 -0700

    load regnet weights

--- hybridnets/modeling/build.py
+++ hybridnets/modeling/build.py
@@ -5,7 +5,10 @@ from .backbone import HybridNetsBackbone
 def build_model(cfg, pretrained=False):
     model = HybridNetsBackbone(cfg)
     if pretrained:
-        weights_path = "./output/hybridnet_saved/checkpoints/hybridnets-d3_199.pth"
+        if cfg.MODEL.BACKBONE.NAME == "efficientnet":
+            weights_path = "./output/hybridnet_saved/checkpoints/hybridnets-d3_199.pth"
+        else:
+            weights_path = "./output/regnet_saved/checkpoints/hybridnets-d3_199.pth"
         model.load_state_dict(torch.load(weights_path))
         print(f"Loaded weights from {weights_path}")
     return model
