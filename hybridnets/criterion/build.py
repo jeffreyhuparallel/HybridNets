90526ff860f2481fff848f358b6f024a5e4e0b28
Jeffrey Hu <jeffrey@moveparallel.com>
2022-10-25 15:37:52 -0700

    delete segmentation loss

--- hybridnets/criterion/build.py
+++ hybridnets/criterion/build.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 import torch.nn as nn
 
-from hybridnets.criterion.loss import FocalLoss, FocalLossSeg, TverskyLoss
+from hybridnets.criterion.loss import FocalLoss
 
 class CriterionCompose(pl.LightningModule):
     def __init__(self, criterions, weights):
@@ -32,30 +32,11 @@ class DetectionLoss(pl.LightningModule):
             "regression_loss": reg_loss,
         }
         return losses
-
-class SegmentationLoss(pl.LightningModule):
-    def __init__(self, cfg):
-        super().__init__()
-        self.seg_criterion1 = TverskyLoss(alpha=0.7, beta=0.3, gamma=4.0/3, from_logits=True)
-        self.seg_criterion2 = FocalLossSeg(alpha=0.25)
-    
-    def __call__(self, inp, target):
-        seg_annot = inp['segmentation']
-        segmentation = target["segmentation"]
-        tversky_loss = self.seg_criterion1(segmentation, seg_annot)
-        focal_loss = self.seg_criterion2(segmentation, seg_annot)
-        seg_loss = tversky_loss + 1 * focal_loss
-        
-        losses = {
-            "segmentation_loss": seg_loss,
-        }
-        return losses
         
         
 def build_criterion(cfg):
     criterions = [
         DetectionLoss(cfg),
-        # SegmentationLoss(cfg),
     ]
     weights = [1, 1]
     
