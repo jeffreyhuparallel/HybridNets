123ee1ffb1a3164f345d523f704a17f0be6f0532
Jeffrey Hu <jeffrey@moveparallel.com>
2022-11-02 13:59:13 -0700

    remove cuda()

--- hybridnets/criterion/focal_loss.py
+++ hybridnets/criterion/focal_loss.py
@@ -19,91 +19,72 @@ def calc_iou(a, b):
 class FocalLoss(nn.Module):
     def __init__(self):
         super().__init__()
+        self.alpha = 0.25
+        self.gamma = 2.0
 
     def forward(self, inp, target):
         boxes = inp["detection_boxes"]
         labels = inp["detection_labels"]
-        regressions = target["regression"]
-        classifications = target["classification"]
         anchors = target["anchors"]
-        
-        alpha = 0.25
-        gamma = 2.0
-        batch_size = classifications.shape[0]
+        classifications = target["classification"]
+        regressions = target["regression"]
+
+        anchor_widths = anchors[0, :, 3] - anchors[0, :, 1]
+        anchor_heights = anchors[0, :, 2] - anchors[0, :, 0]
+        anchor_ctr_x = anchors[0, :, 1] + 0.5 * anchor_widths
+        anchor_ctr_y = anchors[0, :, 0] + 0.5 * anchor_heights
+
         classification_losses = []
         regression_losses = []
-
-        anchor = anchors[0, :, :]  # assuming all image sizes are the same, which it is
-        dtype = anchors.dtype
-
-        anchor_widths = anchor[:, 3] - anchor[:, 1]
-        anchor_heights = anchor[:, 2] - anchor[:, 0]
-        anchor_ctr_x = anchor[:, 1] + 0.5 * anchor_widths
-        anchor_ctr_y = anchor[:, 0] + 0.5 * anchor_heights
-
-        for j in range(batch_size):
-            classification = classifications[j, :, :]
+        for j in range(classifications.shape[0]):
             regression = regressions[j, :, :]
+            classification = classifications[j, :, :]
+            classification = torch.clamp(classification, 1e-4, 1.0 - 1e-4)
             
             box = boxes[j]
             label = labels[j]
             box = box[label != 0]
             label = label[label != 0] - 1
             bbox_annotation = torch.cat([box, label.unsqueeze(dim=1)], dim=1)
-
-            classification = torch.clamp(classification, 1e-4, 1.0 - 1e-4)
-
             if bbox_annotation.shape[0] == 0:
-                alpha_factor = torch.ones_like(classification) * alpha
-                alpha_factor = alpha_factor.cuda()
+                alpha_factor = torch.ones_like(classification) * self.alpha
                 alpha_factor = 1. - alpha_factor
                 focal_weight = classification
-                focal_weight = alpha_factor * torch.pow(focal_weight, gamma)
+                focal_weight = alpha_factor * torch.pow(focal_weight, self.gamma)
 
                 bce = -(torch.log(1.0 - classification))
-
                 cls_loss = focal_weight * bce
 
-                regression_losses.append(torch.tensor(0).to(dtype).cuda())
-                classification_losses.append(cls_loss.sum())
+                classification_loss = cls_loss.sum()
+                classification_losses.append(classification_loss)
+                regression_loss = torch.zeros_like(classification_loss)
+                regression_losses.append(regression_loss)
                 continue
 
-            IoU = calc_iou(anchor[:, :], bbox_annotation[:, :4])
-            
+            IoU = calc_iou(anchors[0, :, :], bbox_annotation[:, :4])
             IoU_max, IoU_argmax = torch.max(IoU, dim=1)
-           
-
-            targets = torch.zeros_like(classification)
-            targets = targets.cuda()
             
             assigned_annotations = bbox_annotation[IoU_argmax, :]
-            
-            positive_indices = torch.full_like(IoU_max,False,dtype=torch.bool) #torch.ge(IoU_max, 0.2) 
-                        
             tensorA = (assigned_annotations[:, 2] - assigned_annotations[:, 0]) * (assigned_annotations[:, 3] - assigned_annotations[:, 1]) > 10 * 10
-
-            
+            positive_indices = torch.full_like(IoU_max,False,dtype=torch.bool)
             positive_indices[torch.logical_or(torch.logical_and(tensorA,IoU_max >= 0.5),torch.logical_and(~tensorA,IoU_max >= 0.15))] = True
-
             num_positive_anchors = positive_indices.sum()
+            
+            targets = torch.zeros_like(classification)
             targets[positive_indices, assigned_annotations[positive_indices, 4].long()] = 1
     
-            alpha_factor = torch.ones_like(targets) * alpha
-            alpha_factor = alpha_factor.cuda()
-
+            alpha_factor = torch.ones_like(targets) * self.alpha
             alpha_factor = torch.where(torch.eq(targets, 1.), alpha_factor, 1. - alpha_factor)
             focal_weight = torch.where(torch.eq(targets, 1.), 1. - classification, classification)
-            focal_weight = alpha_factor * torch.pow(focal_weight, gamma)
+            focal_weight = alpha_factor * torch.pow(focal_weight, self.gamma)
 
             bce = -(targets * torch.log(classification) + (1.0 - targets) * torch.log(1.0 - classification))
-
             cls_loss = focal_weight * bce
 
             zeros = torch.zeros_like(cls_loss)
-            zeros = zeros.cuda()
             cls_loss = torch.where(torch.ne(targets, -1.0), cls_loss, zeros)
-
-            classification_losses.append(cls_loss.sum() / torch.clamp(num_positive_anchors.to(dtype), min=1.0))
+            classification_loss = cls_loss.sum() / torch.clamp(num_positive_anchors, min=1.0)
+            classification_losses.append(classification_loss)
 
             if positive_indices.sum() > 0:
                 assigned_annotations = assigned_annotations[positive_indices, :]
@@ -131,16 +112,17 @@ class FocalLoss(nn.Module):
 
                 regression_diff = torch.abs(targets - regression[positive_indices, :])
 
-                regression_loss = torch.where(
+                reg_loss = torch.where(
                     torch.le(regression_diff, 1.0 / 9.0),
                     0.5 * 9.0 * torch.pow(regression_diff, 2),
                     regression_diff - 0.5 / 9.0
                 )
-                regression_losses.append(regression_loss.mean())
+                regression_loss = reg_loss.mean()
+                regression_losses.append(regression_loss)
             else:
-                regression_losses.append(torch.tensor(0).to(dtype).cuda())
+                regression_loss = torch.zeros_like(classification_loss)
+                regression_losses.append(regression_loss)
                     
         cls_loss = torch.stack(classification_losses).mean(dim=0, keepdim=True)
         det_loss = torch.stack(regression_losses).mean(dim=0, keepdim=True) * 50 # https://github.com/google/automl/blob/6fdd1de778408625c1faf368a327fe36ecd41bf7/efficientdet/hparams_config.py#L233
         return cls_loss, det_loss
-                 
